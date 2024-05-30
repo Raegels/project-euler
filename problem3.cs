daa3223a13625e0b552e43da4d4968a89b15cee1
Rae <raeodijk@gmail.com>
2024-05-30 16:17:43 +0200

    Upload answer problem 3

---
+++ problem3.cs
@@ -0,0 +1,19 @@
+﻿// The prime factors of 13195 are 5, 7, 13 and 29.
+// What is the largest prime factor of the number 600851475143
+
+long number = 600851475143;
+
+int i = 2;
+
+while (i * i < number)
+{
+    while (number % i == 0)
+    {
+        number /= i;
+    }
+    i++;
+    Console.WriteLine(number);
+}
+
+
+
